d89ab98846de0b95ea3701d99cee8c332f552724
jaypandya1 <71070842+jaypandya1@users.noreply.github.com>
2022-05-12 01:31:27 -0400

    using UseEffect and UseState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const [resourceType,setResourseType] = useState('posts');
+  const [resourceType,setResourceType] = useState('posts');
+  const [items,setItems] = useState([]);
+
+  useEffect(() => {
+fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+.then((res) => res.json())
+.then((json) =>  setItems(json))
+  },[resourceType])
   return (
     <>
-    <div className="App">
-     <button onClick={() => {setResourseType('posts') }}>Posts</button>
-     <button onClick={() => {setResourseType('users') }}>Users</button>
-     <button onClick={() => {setResourseType('comments') }}>Comments</button>
+    
+     <button onClick={() => {setResourceType('posts') }}>Posts</button>
+     <button onClick={() => {setResourceType('users') }}>Users</button>
+     <button onClick={() => {setResourceType('comments') }}>Comments</button>
 
-    </div>
+    
     <h1>{resourceType}</h1>
+    {items &&
+      items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })
+    }
     </>
   );
 }
